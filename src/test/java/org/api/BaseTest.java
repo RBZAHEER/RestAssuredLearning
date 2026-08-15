c186162e29f1fe0b569146502c4e920bc7df786b
RBZAHEER <zmulani95@gmail.com>
2026-08-15 15:27:42 +0530

    Post done

--- src/test/java/org/api/BaseTest.java
+++ src/test/java/org/api/BaseTest.java
@@ -1,4 +1,30 @@
 package org.api;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static org.hamcrest.Matchers.containsString;
+
 public class BaseTest {
+
+    protected RequestSpecification requestSpecification;
+    protected ResponseSpecification responseSpecification;
+
+    public BaseTest() {
+
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://jsonplaceholder.typicode.com")
+                .addHeader("Accept", "application/json")
+                .build();
+
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectHeader(
+                        "Content-Type",
+                        containsString("application/json")
+                )
+                .build();
+    }
 }
