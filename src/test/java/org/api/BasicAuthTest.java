a33140d12fea2425563657a45ec1129f4193b18e
Mahemud <zmulani95@gmail.com>
2026-08-23 15:50:11 +0530

    Basic auth updated

--- src/test/java/org/api/BasicAuthTest.java
+++ src/test/java/org/api/BasicAuthTest.java
@@ -1,4 +1,71 @@
 package org.api;
+import org.testng.annotations.Test;
 
-public class BasicAuthTest {
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class BasicAuthTest extends BaseTest{
+
+    //Api Key reqres_db6fedc2758c40749e0f026a8a542097
+    @Test
+    public void basicAuth(){
+        given()
+                .baseUri("https://httpbin.org")
+                .auth()
+                .basic("zaheer","test123")
+
+                .when().get("/basic-auth/zaheer/test123")
+
+                .then()
+                .statusCode(200)
+                .body("authenticated", equalTo(true))
+                .log().all();
+    }
+
+    @Test
+    public void getUserWithAPIKey(){
+        String apikey = "reqres_db6fedc2758c40749e0f026a8a542097";
+
+        given().baseUri("https://reqres.in")
+                .header("x-api-key", apikey)
+
+                .when().get("/api/users?page=2")
+
+                .then().statusCode(200)
+                .log().all();
+
+
+    }
+
+    //Do not test this just for ref
+    @Test
+    public void bearerTokenRequest() {
+
+        String loginRequest = "daff";
+        //4. Extracting a Bearer Token
+        String token =
+                given()
+                        .contentType("application/json")
+                        .body(loginRequest)
+
+                        .when()
+                        .post("/login")
+
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .path("token");
+
+
+        given()
+                .auth()
+                .oauth2(token)
+
+                .when()
+                .get("/protected-resource")
+
+                .then()
+                .statusCode(200);
+
+    }
 }
--- src/test/java/org/api/NegativeTesting.java
+++ src/test/java/org/api/NegativeTesting.java
@@ -1,4 +1,18 @@
-package org.api.pojo;
+package org.api;
 
-public class NegativeTesting {
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class NegativeTesting extends BaseTest{
+    @Test
+    public void getNonExistingPost(){
+        given().spec(requestSpecification)
+                .pathParam("postId",99999)
+
+                .when()
+                .get("/posts/{postId}")
+
+                .then().statusCode(404).log().all();
+    }
 }
